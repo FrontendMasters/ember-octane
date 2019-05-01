0caa6d0eda41a723296033f5976d57cb69a3b7b3
Mike North <michael.l.north@gmail.com>
2019-05-01 13:11:25 -0500

    step8
    Tracked properties and derived state

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,15 +1,32 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
+  @tracked
+  userId = null;
+
+  get isDisabled() {
+    return !this.userId;
+  }
+
   loginAsUserWithId(val) {
     console.log('UserId: ', val);
   }
+
+  /**
+   *
+   * @param {Event & { target: HTMLSelectElement }} evt
+   */
+  @action
+  onSelectChanged(evt) {
+    this.userId = evt.target.value;
+  }
+
   /**
    *
    * @param {Event & {target: HTMLFormElement}} evt
    */
-
   @action
   onLoginFormSubmit(evt) {
     const { target } = evt;
