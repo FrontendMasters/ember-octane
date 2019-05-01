cad039cf579c5bb7d368d2b6759d4b300e0ab610
Mike North <michael.l.north@gmail.com>
2019-05-01 17:27:51 -0500

    step20
    Server rendering with FastBoot

--- app/services/auth.js
+++ app/services/auth.js
@@ -2,6 +2,7 @@ import Service from '@ember/service';
 import { inject as service } from '@ember/service';
 import Router from '@ember/routing/router';
 import { action } from '@ember/object';
+import CookiesService from 'ember-cookies/services/cookies';
 
 const AUTH_KEY = 'shlack-userid';
 
@@ -11,18 +12,23 @@ export default class AuthService extends Service {
    */
   @service router;
 
+  /**
+   * @type {CookiesService}
+   */
+  @service cookies;
+
   get currentUserId() {
-    return window.localStorage.getItem(AUTH_KEY);
+    return this.cookies.read(AUTH_KEY);
   }
 
   loginWithUserId(userId) {
-    window.localStorage.setItem(AUTH_KEY, userId);
+    this.cookies.write(AUTH_KEY, userId);
     this.router.transitionTo('teams');
   }
 
   @action
   logout() {
-    window.localStorage.removeItem(AUTH_KEY);
+    this.cookies.write(AUTH_KEY, null);
     this.router.transitionTo('login');
   }
 }
