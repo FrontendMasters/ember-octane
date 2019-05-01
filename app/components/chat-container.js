ba96104a13d9e97e0abd6b0060e9d13a9a6976f1
Mike North <michael.l.north@gmail.com>
2019-05-01 17:15:51 -0500

    step18
    Deleting chat messages

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -26,6 +26,19 @@ export default class ChatContainerComponent extends Component {
   }
 
   @action
+  async deleteMessage(messageId) {
+    const resp = await fetch(`/api/messages/${messageId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const messageIds = this.messages.map(m => m.id);
+    const idxToDelete = messageIds.indexOf(messageId);
+    this.messages.splice(idxToDelete, 1);
+    this.messages = this.messages; // necesary for @tracked
+  }
+  @action
   async createMessage(body) {
     const {
       channel: { id: channelId, teamId },
